545130fb92b47d5b89b194b449382908e36bc3d9
CallyCa <blackyrite@hotmail.com>
2020-09-11 20:19:24 -0300

    Configurado as pages/controllers/index dos Articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get("/articles", (req,res) => {
-    res.send("Rota de artigos")
+router.get("/admin/articles", (req,res) => {
+    Article.findAll({
+        include: [{model: Category}]
+    }).then((articles) => { 
+        res.render("admin/articles/index", {articles: articles})
+
+    });
 
 });
 
@@ -14,4 +21,57 @@ router.get("/admin/articles/new", (req, res) => {
     
 })
 
-module.exports = router;
+router.post("/articles/save", (req,res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        Slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() =>{
+        res.redirect("/admin/articles")
+    });
+});
+
+router.post("/articles/delete", (req,res) => {
+    var id = req.body.id;
+
+    if(id != undefined){
+        if(!isNaN(id)){ // isNaN verifica se um id sera um numero ou nao
+            Article.destroy({ // Aqui ira deletar um id no DB
+                where: {
+                    id: id
+                }
+            }).then(() =>{
+                res.redirect("/admin/articles")
+            });
+        }else{ // Nao for numero
+            res.redirect("/admin/articles");
+        }
+    }else{ // Null
+        res.redirect("/admin/articles");
+    }
+});
+
+router.get("/admin/articles/edit/:id", (req,res) => {
+    var id = req.params.id;
+    Article.findByPk(id).then(article => {
+
+        if(article != undefined){
+            
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", {categories: categories})
+            });
+
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
+
+module.exports = router; 
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const articlesController = require("./articles/ArticlesController");
 const Article = require("./articles/Article");
 const Category = require("./categories/Category")
 
+
 //View engine
 
 app.set('view engine', 'ejs');
@@ -38,7 +39,57 @@ app.use("/", articlesController);
 
 // configurando Rotas
 app.get("/", (req,res) => {
-    res.render("index");
+    Article.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then(articles => {
+        Category.findAll().then(categories => {
+            res.render("index", {articles: articles, categories: categories});
+        }); 
+    });
+})
+
+app.get("/:slug", (req,res) => {
+    var slug = req.params.slug;
+    Article.findOne({
+        where:{
+            slug: slug
+        }
+    }).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("article", {article: article, categories: categories});
+            }); 
+
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    })
+})
+
+app.get("/category/:slug", (req,res) => {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then(category => {
+        if(category != undefined){
+            Category.findAll().then(categories => {
+                res.render("index",{articles: category.articles, categories: categories})
+            })
+
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    })
+
 })
 
 app.listen(3000, () => {
